f6ec3e45e7b98a142ae3d63a919db1a15d4ed994
Heigler Rocha <lordheigler@gmail.com>
2012-09-29 17:35:54 -0300

    new gunicorn settings

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,16 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python 
 import sys 
 import os
+import multiprocessing
 from django.conf import settings
-try:
-    import settings # Assumed to be in the same directory.
-    sys.path.insert(0, os.path.join(settings.PROJECT_ROOT, "apps"))
-except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import settings
+
+logdir = os.path.join(settings.PROJECT_ROOT, '..', 'deploy')
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+
 bind = '127.0.0.1:9000'
-workers = 3 
-worker_connections = 2048
-worker_class = 'egg:gunicorn#sync'
-logfile = '/tmp/gunicorn_djangoproject.log'
-timeout = 3600
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_connections = 1000
+worker_class = 'egg:gunicorn#gevent'
+logfile = os.path.join(logdir, 'gunicorn.log')
+timeout = 60
+keepalive = 2
+limit_request_line = 2040
+limit_request_fields = 100
+debug = False
+pidfile = os.path.join(logdir, 'pidfile.pid')
+loglevel = 'error'
+
+# for development stage, you can import this file and change anything you
+# want like in settings.py
